207e080180085444c35d3d7d5467c98ff10b4b9d
Deepak Sharma <115694930+Deepak5113@users.noreply.github.com>
2024-07-09 23:24:27 +0530

    Time: 163 ms (40.86%), Space: 27.8 MB (93.78%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -3,6 +3,7 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        for i in range(k):
-            nums.insert(0,nums[-1])
-            del nums[-1]
+        k = k % len(nums)
+        new = nums[-k:] + nums[:-k]
+        nums.clear()
+        nums.extend(new)
