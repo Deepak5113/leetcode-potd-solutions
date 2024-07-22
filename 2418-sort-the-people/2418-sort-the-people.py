73fd65d6b8b16dc2337eac640d44da2916084b1a
Deepak Sharma <115694930+Deepak5113@users.noreply.github.com>
2024-07-22 14:56:14 +0530

    Time: 102 ms (70.62%), Space: 17.2 MB (14.55%) - LeetHub

---
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -0,0 +1,4 @@
+class Solution:
+    def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+        return [name for name,height in sorted(zip(names,heights),key=lambda x:x[1],reverse=True)]
+
